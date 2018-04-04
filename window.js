172b32d040d9041e509019cb41c172c6932af418
Mark Freese <mark@zappmonkey.com>
2018-04-04 07:24:37 +0200

    Correct handling of terminal queries / Better terminal handling / Added archive / Added copy action to archive

--- window.js
+++ window.js
@@ -44,7 +44,7 @@ function getDatabases() {
     $('#menu .top .button.databases-but').addClass('active').show();
     runQuery('SHOW DATABASES', function (err, results, fields) {
         if (err) {
-            throw err;
+            return parseError(err);
         }
         var databases = {system: [], user: []};
         for (var i=0; i<results.length; i++) {
@@ -67,6 +67,19 @@ function getDatabases() {
     });
 };
 
+function parseError(err) {
+    addNotification("Error in query", err.message, "error");
+    return err.message;
+}
+
+$(document).on('click',  '#notifications .notification', function(e) {
+    $(this).remove();
+});
+
+function addNotification(title, content, type) {
+    $("#notifications").prepend(notulous.util.renderTpl("notification", {title: title, content: content, type: type}));
+};
+
 $(document).on('click',  '#list .tables li', function(e) {
     getTable($(this).text());
     $('#list .tables li.active').removeClass('active');
@@ -85,10 +98,9 @@ function getTable(table, page, sort, order) {
     query += " LIMIT " + start + ", " + limit + ";";
     runQuery(query , function (err, records, fields) {
         if (err) {
-            throw err;
+            return parseError(err);
         }
-        $("#workspace .content").html(
-            notulous.util.renderTpl("table", {
+        var html = notulous.util.renderTpl("table", {
                 fields: fields,
                 records: records,
                 table: table,
@@ -96,7 +108,12 @@ function getTable(table, page, sort, order) {
                 sort: sort,
                 order: order
             }
-        ));
+        );
+        if ($("#workspace .content > .table").length == 0) {
+            $("#workspace .content").append(html);
+        } else {
+            $("#workspace .content > .table").replaceWith(html);
+        }
         $("#workspace .content table th").on("click", function() {
             var order = $(this).data("order");
             if (notulous.util.empty(order)) {
@@ -128,12 +145,12 @@ function getTable(table, page, sort, order) {
 function __getTable(data) {
     runQuery("SHOW TABLE STATUS WHERE `Name` = '" + data.table + "';" , function (err, records, fields) {
         if (err) {
-            throw err;
+            return parseError(err);
         }
         data.count = records[0].Rows;
         runQuery({sql:data.query, typeCast:false}, function (err, records, fields) {
             if (err) {
-                throw err;
+                return parseError(err);
             }
             data.fields = fields;
             data.records = records;
@@ -148,21 +165,21 @@ function __getTable(data) {
             data.start += 1;
             var html = notulous.util.renderTpl("table", data);
             $("#workspace .content > div").hide();
-            if ($("#workspace .content .table").length > 0) {
-                $("#workspace .content .table").replaceWith(html);
-                $("#workspace .content .table").show();
+            if ($("#workspace .content > .table").length > 0) {
+                $("#workspace .content > .table").replaceWith(html);
+                $("#workspace .content > .table").show();
             } else {
                 $("#workspace .content").append(html);
             }
-            $("#workspace .content .table th").on("click", function() {
+            $("#workspace .content > .table th").on("click", function() {
                 var order = $(this).data("order");
                 if (notulous.util.empty(order)) {
                     order = 'desc';
                 }
-                getTable($("#workspace .content .table").data("table"), undefined, $(this).data("sort"), order);
+                getTable($("#workspace .content > .table table").data("table"), undefined, $(this).data("sort"), order);
             });
-            $("#workspace .content .status .next, #workspace .content .status .previous").on("click", function() {
-                var table = $("#workspace .content .table");
+            $("#workspace .content > .table .status .next, #workspace .content > .table .status .previous").on("click", function() {
+                var table = $("#workspace .content > .table");
                 getTable(table.data("table"), $(this).data("page"), table.data("sort"), table.data("order"));
             });
         });
@@ -174,12 +191,13 @@ $(document).on("dblclick", ".top", function(e) {
 });
 
 $(document).on('click',  '#menu .databases li', function(e) {
-    $("#workspace .content").html("");
+    $("#workspace .content > div").hide();
+    $("#workspace .top .buttons.terminal").hide();
     $("#workspace .top .buttons.database").show();
     runQuery('USE `' + $(this).text() + '`');
     runQuery('SHOW TABLES;' , function (err, results, fields) {
         if (err) {
-            throw err;
+            return parseError(err);
         }
         $("#list .content").html(notulous.util.renderTpl("tables", {tables: results}))
     });
@@ -222,18 +240,37 @@ $(document).on('click',  '#workspace .top .database .terminal', function(e) {
             // theme: "tomorrow-night-bright",
             indentWithTabs: false,
             lineWrapping: true,
-            smartIndent: true,
             lineNumbers: true,
             matchBrackets : true,
             autofocus: true,
         });
         var sizes = notulous.storage.get("terminal");
-        console.log(sizes);
         if (sizes) {
-            $("#workspace .terminal .editor-container").height(sizes.editor),
-            $("#workspace .terminal .results").height(sizes.results)
+            $("#workspace .content .terminal .editor-container").height(sizes.editor),
+            $("#workspace .content .terminal .results").height($("#workspace .content .terminal").height() - sizes.editor - 1);
         };
     }
+});
+
+$(document).on('click',  '#workspace .top .database .table', function(e) {
+    $("#workspace .content > div").hide();
+    $("#workspace .content > .table").show();
+});
+
+$(document).on('click',  '#workspace .top .database .archive', function(e) {
+    $("#workspace .content > div").hide();
+    var history = notulous.storage.get("history");
+    var html = notulous.util.renderTpl("archive", {archive: history});
+    if ($("#workspace .content > .archive").length > 0) {
+        $("#workspace .content > .archive").replaceWith(html);
+        $("#workspace .content > .archive").show();
+    } else {
+        $("#workspace .content").append(html);
+        $("#workspace .content .archive .action-copy").on('click', function() {
+            copyToClipboard($(this).parent().parent().find(".query"));
+        });
+    }
+
 });
 
 $(document).on('click',  '#workspace .top .buttons.terminal .run', function(e) {
@@ -242,14 +279,15 @@ $(document).on('click',  '#workspace .top .buttons.terminal .run', function(e) {
 
 function getAndRunCustomQuery() {
     var queries = getEditorQueries();
-    if (!queries) {
-        return;
-    } else if (queries.length == 1) {
-        runCustomQuery(queries[0]);
-    }
     var hasSelection = false;
     if (window.editor.getSelection().trim().length > 4) {
         hasSelection = true;
+    } else {
+        if (!queries) {
+            return;
+        } else if (queries.length == 1) {
+            return runCustomQuery(queries[0]);
+        }
     }
     $("body").append(
         notulous.util.renderTpl("queries-terminal", {queries: queries, hasSelection: hasSelection})
@@ -306,7 +344,7 @@ function runCustomQuery(query, sort, order) {
     __lastCustomQuery = query;
     runQuery({sql:query, typeCast:false} , function (err, records, fields) {
         if (err) {
-            throw err;
+            return parseError(err);
         }
         $("#workspace .content .results").html(
             notulous.util.renderTpl("table", {
@@ -418,3 +456,11 @@ $(document).ready(function() {
         resizeWindow();
     }
 });
+
+function copyToClipboard(el) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(el.text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+};
